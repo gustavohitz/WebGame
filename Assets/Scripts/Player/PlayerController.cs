98953ca74de8c38978e2a4d9daf6ff42c46e92ac
Gustavo Hitzschky <gust.campos@gmail.com>
2022-12-14 11:20:28 -0300

    Adding animation to player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float speed = 10f;
+
     void Update() {
         float xAxis = Input.GetAxis("Horizontal");
         float zAxis = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(xAxis, 0, zAxis);
 
-        transform.Translate(direction);
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if(direction != Vector3.zero) {
+            GetComponent<Animator>().SetBool("Running", true);
+        }
+        else {
+            GetComponent<Animator>().SetBool("Running", false);
+        }
     }
 
 }
