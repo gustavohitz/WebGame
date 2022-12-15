a1111c5b10ede3a00fdfb7c83cd81620a0c7b137
Gustavo Hitzschky <gust.campos@gmail.com>
2022-12-15 17:50:35 -0300

    Adding trigger to bullet, destroying enemies and the bullet

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -6,10 +6,18 @@ public class Bullet : MonoBehaviour {
 
     public float speed = 20f;
     public float timeToDestroy = 3f;
+    public string tagToCheck = "Enemy";
     
     void FixedUpdate() {
         GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.forward * speed * Time.deltaTime);
         Destroy(gameObject, timeToDestroy);
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == tagToCheck) {
+            Destroy(other.gameObject);
+        }
+        
+        Destroy(gameObject);
     }
 
 }
