cbb2f4bb22545b8041d6028ee342cd9453c88d8c
Gustavo Hitzschky <gust.campos@gmail.com>
2022-12-29 08:18:47 -0300

    Registering surviving time

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour {
 
     public Slider lifeSlider;
     public GameObject GameOverPanel;
+    public TextMeshProUGUI survivingTimeTxt;
 
     private PlayerController playerControllerScript;
 
@@ -26,6 +28,11 @@ public class UIManager : MonoBehaviour {
     public void ShowGameOverPanel() {
         GameOverPanel.SetActive(true);
         Time.timeScale = 0;
+
+        int minutes = (int)(Time.timeSinceLevelLoad / 60); //os parênteses convertem o que vem depois para inteiro
+        int seconds = (int)(Time.timeSinceLevelLoad % 60); //a porcentagem (módulo) guarda o resto da divisão
+
+        survivingTimeTxt.text = "You have survived for " + minutes + "min and " + seconds + "s";  
     }
     public void Restart() {
         SceneManager.LoadScene("game");
